ebed2de7df9d998b7610dbf4d7f9a01813e8a9b0
LahRee <Laurie.Strecker@gmail.com>
2019-08-08 17:30:47 -0700

    messing with logic

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,11 @@
+//USE AJAX to get API info
+//Define variables, array of strings 
+//var topics = ["sad", "happy", "excited", "bored", "amused", "entertained"];
+//var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+//Make a loop that appends a button to each string in the array
+//When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+//Under every gif, display its rating (PG, G, so on).
+$(document).ready(function)() {
+var topics = [
